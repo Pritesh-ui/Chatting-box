5b55322d6317c2ba7d15c2e85a24958837b11fb4
pritesh <priteshsr98@gmail.com>
2023-07-30 19:50:43 -0700

    second commit

--- config/db.js
+++ config/db.js
@@ -3,11 +3,14 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://pritesh:<password>@cluster0.yqox1rp.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      }
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
